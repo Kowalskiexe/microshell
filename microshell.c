0616640d2ae67679244859085c7a5b86a79a0f3a
Kowalskiexe <kowalski.exe@gmail.com>
2022-11-02 19:39:55 +0100

    executing commands, freeing buffer

--- microshell.c
+++ microshell.c
@@ -1,14 +1,17 @@
+#include <ctype.h>
+#include <errno.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
-#include <stdbool.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 const int max_word_count = 1000;
 const int max_word_length = 1000; // including null terminator
 
 // on success returns number of loaded words
-int read_input(char *const* buff) {
+int read_input(char *const *buff) {
     int word_count = 0;
     int idx = 0;
     char c;
@@ -17,6 +20,7 @@ int read_input(char *const* buff) {
         if (isspace(c)) {
             // mark word
             if (idx > 0) {
+                buff[word_count][idx] = '\0';
                 word_count++;
                 idx = 0;
             }
@@ -26,6 +30,18 @@ int read_input(char *const* buff) {
         }
     } while (c != EOF && c != '\n');
     return word_count;
+}
+
+// has side effects, adds NULL at the end of the buff
+void execute_command(char *name, char **args, const int args_count) {
+    pid_t id = fork();
+    if (id == 0) {
+        args[args_count] = NULL;
+        execvp(name, args);
+        printf("Error number: %d\n", errno);
+    } else {
+        wait(NULL);
+    }
 }
 
 void prompt() {
@@ -42,14 +58,17 @@ int main() {
             buff[i] = malloc(max_word_length * sizeof(char));
 
         int count = read_input(buff);
-        printf("%d words\n", count);
-        for (int i = 0; i < count; i++)
-            printf("%s\n", buff[i]);
+
+        execute_command(buff[0], buff, count);
 
         if (strcmp(buff[0], "exit") == 0) {
             printf("exit command read, exiting...\n");
             return 0;
         }
+
+        for (int i = 0; i < max_word_count; i++)
+            free(buff[i]);
+        free(buff);
     }
 
     return 0;
